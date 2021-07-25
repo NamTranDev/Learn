152617262cc040b33f998ad9c6ab9f87705b53c1
NamTranDev <namtran09061992@gmail.com>
2021-07-25 11:09:51 +0700

    Learn : - Tapping into Widget Properties

--- Flutter/mi_card_flutter/mi_card_flutter/lib/main.dart
+++ Flutter/mi_card_flutter/mi_card_flutter/lib/main.dart
@@ -12,29 +12,22 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(height: double.infinity, width: 100, color: Colors.red),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    height: 100,
-                    width: 100,
-                    color: Colors.yellow,
-                  ),
-                  Container(
-                    height: 100,
-                    width: 100,
-                    color: Colors.yellow.withOpacity(0.5),
-                  )
-                ],
-              ),
-              Container(height: double.infinity, width: 100, color: Colors.blue)
-            ],
-          ),
-        ),
+            child: Column(
+          children: [
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: NetworkImage(
+                  "https://i2.wp.com/twinfinite.net/wp-content/uploads/2019/11/Final-Fantasy-VII-Remake-Cloud-1.jpg?w=996&h=996&crop=1&ssl=1"),
+            ),
+            Text(
+              "Cloud",
+              style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            ),
+          ],
+        )),
         floatingActionButton: FloatingActionButton(
           backgroundColor: Colors.red,
           onPressed: () {},
