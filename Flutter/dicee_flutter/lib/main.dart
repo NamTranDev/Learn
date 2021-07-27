f2f25950475d001d9b20b4027e2af91312eb7bdd
NamTranDev <namtran09061992@gmail.com>
2021-07-27 10:54:05 +0700

    Learn : Dicee - Building Apps with State - Using the Expanded Widget to Create Flexible Layouts - How to Use Intention Actions - Adding Gesture Detection with Flutter Button Widges

---
+++ Flutter/dicee_flutter/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+              flex: 1,
+              child: FlatButton(
+                child: Image.asset("images/dice1.png"),
+                onPressed: () {
+                  print("Image 1 press");
+                },
+              )),
+          Expanded(
+              flex: 1,
+              child: FlatButton(
+                child: Image.asset("images/dice1.png"),
+                onPressed: () {
+                  print("Image 2 press");
+                },
+              )),
+        ],
+      ),
+    );
+  }
+}
