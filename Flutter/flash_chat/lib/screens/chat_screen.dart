77fd86a6e40a6c719f40d8098e46c002c7d9d76a
NamTranDev <namtran09061992@gmail.com>
2021-08-29 11:34:42 +0700

    Learn : - [Dart] Streams - Turning Streams into Widgets Using the StreamBuilder

--- Flutter/flash_chat/lib/screens/chat_screen.dart
+++ Flutter/flash_chat/lib/screens/chat_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flash_chat/constants.dart';
@@ -10,12 +11,33 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
-  var _auth = FirebaseAuth.instance;
+  final _store = FirebaseFirestore.instance;
+  final _auth = FirebaseAuth.instance;
+
+  String? message;
 
   @override
   void initState() {
     super.initState();
     print(_auth.currentUser);
+    loadMessages();
+  }
+
+  void loadMessages() async {
+    // final messages = await _store.collection("messages").get();
+    // messages.docs.forEach((element) {
+    //   print(element.data());
+    // });
+    // await for (var snapshot in _store.collection("messages").snapshots()) {
+    //   snapshot.docs.forEach((element) {
+    //     print(element.data());
+    //   });
+    // }
+    _store.collection("messages").snapshots().listen((event) {
+      event.docs.forEach((element) {
+        print(element.data());
+      });
+    });
   }
 
   @override
@@ -26,7 +48,7 @@ class _ChatScreenState extends State<ChatScreen> {
         actions: <Widget>[
           IconButton(
               icon: Icon(Icons.close),
-              onPressed: () {
+              onPressed: () async {
                 _auth.signOut();
                 Navigator.pop(context);
               }),
@@ -39,6 +61,29 @@ class _ChatScreenState extends State<ChatScreen> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
+            StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return Center(
+                    child: Text("Not found chat data"),
+                  );
+                }
+                if (snapshot.hasError) {
+                  return Center(
+                    child: Text("Error : ${snapshot.error}"),
+                  );
+                }
+                List<Text> listMessages = [];
+                snapshot.data?.docs.forEach((element) {
+                  listMessages.add(Text(
+                      "${element.data()["sender"]}  send  ${element.data()["text"]}"));
+                });
+                return Column(
+                  children: listMessages,
+                );
+              },
+              stream: _store.collection("messages").snapshots(),
+            ),
             Container(
               decoration: kMessageContainerDecoration,
               child: Row(
@@ -47,14 +92,18 @@ class _ChatScreenState extends State<ChatScreen> {
                   Expanded(
                     child: TextField(
                       onChanged: (value) {
-                        //Do something with the user input.
+                        message = value;
                       },
                       decoration: kMessageTextFieldDecoration,
                     ),
                   ),
                   FlatButton(
                     onPressed: () {
-                      //Implement send functionality.
+                      // message + email
+                      _store.collection("messages").add({
+                        "text": message,
+                        "sender": _auth.currentUser?.email
+                      });
                     },
                     child: Text(
                       'Send',
