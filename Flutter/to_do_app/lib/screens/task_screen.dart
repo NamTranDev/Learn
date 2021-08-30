24b5affaec03fce7a1fa346462e95ee17c26e029
NamTranDev <namtran09061992@gmail.com>
2021-08-30 21:10:15 +0700

    Learn : Flutter State Management

---
+++ Flutter/to_do_app/lib/screens/task_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class TaskScreen extends StatelessWidget {
+  const TaskScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+    ));
+  }
+}
