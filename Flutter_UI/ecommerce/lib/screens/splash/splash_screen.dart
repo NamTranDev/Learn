a4e5eb25c076545a551f2481441533b5b53c40e3
NamTranDev <namtran09061992@gmail.com>
2021-10-13 22:22:47 +0700

    - Update splash screen

--- Flutter_UI/ecommerce/lib/screens/splash/splash_screen.dart
+++ Flutter_UI/ecommerce/lib/screens/splash/splash_screen.dart
@@ -7,10 +7,8 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Body(),
-      ),
+    return Scaffold(
+      body: Body(),
     );
   }
 }
